4767426cdb166637b12cd36a9db43cc5440fc9cc
Jananisoundar <jananisoundar12@gmail.com>
2024-05-12 21:50:44 +0530

    Add files via upload

---
+++ Question_9.py
@@ -0,0 +1,4 @@
+#write a program that takes a string and returns the number of words in it.
+string = "guvi  zen  class"
+Splitted_string = string.split()
+print("Total number of words in the given string:", len(Splitted_string))
